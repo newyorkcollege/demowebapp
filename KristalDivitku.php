ed8e15a3397ed2e51fee3af55515d49edf03d441
Kristal Divitku <krisdivitku@gmail.com>
2018-12-15 14:22:07 +0100

    Update KristalDivitku.php

--- KristalDivitku.php
+++ KristalDivitku.php
@@ -1,1 +1,11 @@
 This is a new file for the given exercise
+<!DOCTYPE html>
+<html>	<head>
+		<title>This is my simple addition to this exercise</title>
+	</head>
+	<body>
+		<?php
+			echo "Hi Everyone, my name is Kristal and this is my php file";
+		?>
+	</body>
+</html>
