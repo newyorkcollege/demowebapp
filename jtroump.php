be66cd5f20dcf638edb679718d38c26c4497e64b
jtroump <jtroump@icloud.com>
2019-01-27 17:22:22 +0200

    This is my first commit.

---
+++ jtroump.php
@@ -0,0 +1,8 @@
+<?php
+  $firstName = 'John';
+  $lastName = 'Troumpadakis';
+  $nickName = 'jtroump';
+
+  echo "Hi! My name is " . $firstName . " " . $lastName . " and I just created a GitHub account.<br/>You can find me with the nickname " . $nickName . ".";
+
+?>
